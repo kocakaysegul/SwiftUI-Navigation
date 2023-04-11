c5fb38669aacc3961401b00b4168eab991cdb2ee
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-11 14:37:41 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Ayşegül Koçak on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
