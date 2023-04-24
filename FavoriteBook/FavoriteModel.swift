6e366edfb1bd894c4b2e7fcb97704eda46b08f90
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-24 12:52:14 +0300

    list

--- FavoriteBook/FavoriteModel.swift
+++ FavoriteBook/FavoriteModel.swift
@@ -25,7 +25,19 @@ struct FavoriteElements : Identifiable {
     
 }
 
+//Foods
 let dessert = FavoriteElements(name: "Browni", imagename: "dessert", description: "Something1 djfjhd")
 let burger = FavoriteElements(name: "Burger", imagename: "burger", description: "Something2 djfjhd")
+let pasta = FavoriteElements(name: "Pasta", imagename: "pasta", description: "Something3 djfjhd")
 
-let favoriteFoods = FavoriteModel(title: "Favorite Foods", elements: [dessert, burger])
+let favoriteFoods = FavoriteModel(title: "Favorite Foods", elements: [dessert, burger, pasta])
+
+
+//Foods
+let pulpfiction = FavoriteElements(name: "pulpfiction", imagename: "pulpfiction", description: "Something4 djfjhd")
+let thedarkknight = FavoriteElements(name: "thedarkknight", imagename: "thedarkknight", description: "Something5 djfjhd")
+let killbill = FavoriteElements(name: "killbill", imagename: "killbill", description: "Something6 djfjhd")
+
+let favoriteMovies = FavoriteModel(title: "Favorite Movies", elements: [pulpfiction, thedarkknight, killbill])
+
+let myFavorites = [favoriteFoods,favoriteMovies]
